66c78919d7f51406a8ef282d53678b6ac484cc49
Aristide lavri <lavri.alain@outlook.com>
2018-12-11 11:46:53 +0000

    Add project files.

---
+++ Social-Matching-Algorithm-core/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Social_Matching_Algorithm_core
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
